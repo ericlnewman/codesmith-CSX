78e1a06fbf6b527497f03031faf80db7a1916343
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-10-08 22:39:13 -0400

    Update reduce.js

--- Callbacks & Higher-order Functions/reduce.js
+++ Callbacks & Higher-order Functions/reduce.js
@@ -1,16 +1,11 @@
-function eitherFilter(array, callback1, callback2) {
-  // ADD CODE HERE
-  let result = [];
-  for(let element of array){
-    if(callback1(element) || callback2(element)){
-      result.push(element);
-    }
+// ADD CODE HERE
+function reduce(array, callback, acc){
+  for(let i = 0; i < array.length; i++){
+    acc = callback(acc, array[i]);
   }
-  return result;
+  return acc;
 }
-
 // Uncomment these to check your work!
-const arrOfNums = [10, 35, 105, 9];
-const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
-const over100 = n => n > 100;
-console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
+const nums = [4, 1, 3];
+const add = function(a, b) { return a + b; }
+console.log(reduce(nums, add, 0)); // should log 8
