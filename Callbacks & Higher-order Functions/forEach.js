1dd65eaf8987e3c7880355ff8e132d59bea8885a
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-10-08 22:05:22 -0400

    Update forEach.js

--- Callbacks & Higher-order Functions/forEach.js
+++ Callbacks & Higher-order Functions/forEach.js
@@ -1,16 +1,16 @@
 // ADD CODE HERE
-function forEach(array, callback){
-  let result = [];
-  for(let element of array){
-   	result.push(callback(element));
+function forEach(arr, callback){
+  for(let i of arr){
+    callback(i);
   }
-  return result;
 }
-function map(array, callback){
-  return forEach(array, callback)
+function map(arr, callback){
+  const result = [];
+  forEach(arr, (i) => {result.push(callback(i));})
+  return result;
 }
 // Uncomment these to check your work!
 console.log(typeof forEach); // should log: 'function'
 forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
 console.log(typeof map); // should log: 'function'
-console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]ea
+console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
