84046003a2844aab417561e9a52e494fa78f3307
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-10-08 22:25:05 -0400

    Update eitherCallback.js

--- Callbacks & Higher-order Functions/eitherCallback.js
+++ Callbacks & Higher-order Functions/eitherCallback.js
@@ -1,16 +1,18 @@
-function eitherFilter(array, callback1, callback2) {
+function eitherCallback(callback1, callback2) {
   // ADD CODE HERE
-  let result = [];
-  for(let element of array){
-    if(callback1(element) || callback2(element)){
-      result.push(element);
-    }
-  }
-  return result;
+  return (n) => {return callback1(n) || callback2(n);}
 }
 
 // Uncomment these to check your work!
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i], i, array)) newArray.push(array[i]);
+  }
+  return newArray;
+}
 const arrOfNums = [10, 35, 105, 9];
 const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
 const over100 = n => n > 100;
-console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
+const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
+console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
