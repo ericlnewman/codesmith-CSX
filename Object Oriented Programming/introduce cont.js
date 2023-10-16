7f6ae55f48e635baa5dcd08b85028f00c0586f40
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-10-16 15:02:28 -0400

    Update introduce cont.js

--- Object Oriented Programming/introduce cont.js
+++ Object Oriented Programming/introduce cont.js
@@ -8,13 +8,13 @@ function personFromConstructor(name, age) {
   const person = new PersonConstructor();
   person.name = name;
   person.age = age;
-  
   return person;
 }
 
 const mike = personFromConstructor('Mike', 30);
 
 // add code here
-PersonConstructor.prototype.introduce = () => console.log("Hi, my name is " + mike.name);
+PersonConstructor.prototype.introduce = function(){console.log(`Hi, my name is ${this.name}`)}
+
 // Uncomment this line to check your work!
  mike.introduce(); // -> Logs 'Hi, my name is Mike'
