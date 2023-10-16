e8a29cf86092c72c1b9e1f21e3c92054888549be
ericlnewman <130870127+ericlnewman@users.noreply.github.com>
2023-10-15 21:32:03 -0400

    Update cycleIterator.js

--- Closure, Scope & Execution Context/cycleIterator.js
+++ Closure, Scope & Execution Context/cycleIterator.js
@@ -1,15 +1,25 @@
 // ADD CODE HERE
+// create a function that has an array as an argument
 function cycleIterator(array){
-  let counter = 0;
-  return ()=>{
-    const element = array[counter];
-    counter++;
-    if(counter === array.length){
-      counter = 0;
+  // create a returned function that does not have any arguments
+  const cache = {};
+  let count = 0;
+  return () => {
+    // the function returns the first element of the array on the first call
+		// returns the second element on the second call, and so on
+    for(let i = 0; i < array.length; i++){
+      cache[i] = array[i];
     }
-    return element;
+    let ans = cache[count];
+    count++;
+    if(count > 2){
+      count = 0;
+    }
+  	return ans;
   }
 }
+	
+
 // Uncomment these to check your work!
 const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
 const getDay = cycleIterator(threeDayWeekend);
